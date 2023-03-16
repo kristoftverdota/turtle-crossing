e5b5e3b65d05ccf29cda4992298a97eba406df87
Kristof Tverdota <tverdota.kristof@gmail.com>
2023-03-16 10:47:49 +0100

    Create player.py
    Player movement and detection of the level completion.

---
+++ player.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.shape("turtle")
+        self.penup()
+        self.go_back()
+        self.setheading(90)
+
+    def go(self):
+        self.forward(MOVE_DISTANCE)
+
+    def go_back(self):
+        self.goto(STARTING_POSITION)
+
+    def finish(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
